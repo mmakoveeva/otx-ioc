8328655c1e17932adfc30cde19c14ef63289cff3
mmakoveeva <mmakoveeva@sfedu.ru>
2021-05-27 17:09:28 +0300

    1

--- API.py
+++ API.py
@@ -5,6 +5,6 @@ API_KEY = "8c3b1fd2add0a2325e7edd39eeb9a0e7cd577238a65fc83f67c4b1d2c08ee625"
 
 api_response = requests.get(f"https://otx.alienvault.com/browse/global?include_inactive=0&sort=-modified&page=1&indicatorsSearch=").text
 
-
+#nkjbbbbbbbbbbbbbbbbb
 
 
